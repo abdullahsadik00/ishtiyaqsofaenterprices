73fae5376b543bae5e2c7c050c3484c44fc3bf24
Sadik Shaikh <abdullahsadik00@gmail.com>
2023-12-21 14:46:56 +0530

    Nav Component

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import Nav from "./section/Nav";
+import Hero from "./section/Hero";
+import PopularProduct from "./section/PopularProduct";
+import Quality from "./section/Quality";
+import Services from "./section/Services";
+import Offer from "./section/Offer";
+import Review from "./section/Review";
+import Subscribe from "./section/Subscribe";
+import Footer from "./section/Footer";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b ">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProduct />{" "}
+      </section>
+      <section className="padding">
+        <Quality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />{" "}
+      </section>
+      <section className="padding">
+        <Offer />
+      </section>
+      <section className="padding bg-pale-blue">
+        <Review />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="padding-x padding-t pb-8 bg-black">
+        <Footer />
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/section/Nav.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { navLinks } from "../constant";
+import { hamburger } from "../assets/icons";
+import { headerLogo } from "../assets/images";
+
+const Nav = () => {
+  console.log(navLinks);
+  return (
+    // <header className="padding-x py-8 absolute z-10 w-full">
+    //   <nav className="flex justify-between items-center max-container">
+    //     <a href="/">
+    //       <img src={headerLogo} alt="s" width={130} height={30} />
+    //     </a>
+
+    //     <ul className="flex flex-1 justify-center items-center gap-16 max-lg:hidden">
+    //       {navLinks.map((el) => (
+    //         <li key={el.label}>
+    //           <a
+    //             href={el.href}
+    //             className="font-montserat leading-normal text-lg text-slate-gray"
+    //           >
+    //             {el.label}
+    //           </a>
+    //         </li>
+    //       ))}
+    //     </ul>
+    //     <div className="hidden max-lg:block">
+    //       <img src={hamburger} alt="hamburger" width={25} height={25} />
+    //     </div>
+    //   </nav>
+    // </header>
+    <header className="padding-x py-3 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="Logo" width={50} height={50} />
+        </a>
+        <ul className="flex flex-1 justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((el) => (
+            <li key={el.label}>
+              <a
+                href={el.href}
+                className="font-montserat leading-normal text-lg text-slate-gray"
+              >
+                {el.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <button className="font-montserat  text-lg text-white bg-bright-orange px-5 py-3 leading-none rounded-full">
+            Sign up
+          </button>
+        </div>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="hamburger" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
